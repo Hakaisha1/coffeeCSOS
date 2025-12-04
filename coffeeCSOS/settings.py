c663613496b59f48d63705c12d3fe6ca323ba113
Hakaisha1 <ixforce4@gmail.com>
2025-12-04 20:08:16 +0700

    Set DEBUG to True for development environment

--- coffeeCSOS/settings.py
+++ coffeeCSOS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)q9@&j+(bhsjz!xfgcfm$cu1rv-hxqmgu_xdq(cngl=$&$h8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['exynos25.pythonanywhere.com']  # Untuk production, ganti dengan domain PythonAnywhere: ['yourusername.pythonanywhere.com']
 
