a17293f0d495adacd6274f0fb76d383abc491510
aisyafebby <aisyafebby40@gmail.com>
2025-12-11 18:11:34 +0700

    customerfiks

--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from logistik.models import Barang
 
 class MenuItem(models.Model):
     nama = models.CharField(max_length=100, unique=True)
@@ -15,15 +17,15 @@ class Customer(models.Model):
         return self.nama
 
 
+
 class Riwayat(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="riwayat")
     total_belanja = models.IntegerField(null=True, blank=True)
     perubahan = models.IntegerField(null=True, blank=True)
-    pesanan = models.JSONField(null=True, blank=True)  # simpan cart sebagai JSON
+    pesanan = models.JSONField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        # ganti self.jenis dengan info lain, misal total_belanja
         return f"Riwayat {self.id} - {self.customer.nama} (Total: Rp{self.total_belanja or 0:,})"
 
 
@@ -46,10 +48,11 @@ class Pesanan(models.Model):
     def __str__(self):
         return f"Pesanan {self.id} - {self.customer.nama}"
 
+
 class MenuItemBahan(models.Model):
     menu_item = models.ForeignKey(MenuItem, related_name='bahan_baku', on_delete=models.CASCADE)
-    barang = models.ForeignKey('logistik.Barang', on_delete=models.PROTECT)
-    jumlah_dibutuhkan = models.IntegerField(default=1)  # Berapa unit per porsi
+    barang = models.ForeignKey(Barang, on_delete=models.PROTECT)
+    jumlah_dibutuhkan = models.IntegerField(default=1)
     
     def kurangi_stok(self, jumlah_pesanan):
         total_unit = self.jumlah_dibutuhkan * jumlah_pesanan
